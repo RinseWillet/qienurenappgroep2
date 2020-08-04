b7ce59927b917b17f5d53b349c2f70ec06bccd53
Kevin Koene <kevinkoene89@gmail.com>
2020-08-04 16:59:25 +0200

    temp

--- src/main/java/app/qienuren/rest/FormulierEndpoint.java
+++ src/main/java/app/qienuren/rest/FormulierEndpoint.java
@@ -2,20 +2,26 @@ package app.qienuren.rest;
 
 import app.qienuren.controller.FormulierService;
 import app.qienuren.model.Formulier;
+import app.qienuren.model.WerkDag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/formulier")
 public class FormulierEndpoint {
 
-    @Autowired
+//    @Autowired
     FormulierService formulierService;
 
     @PostMapping("/nieuw")
     public Formulier nieuwFormulier(@RequestBody Formulier formulier) {
-        System.out.println(formulier.getWerkDagen());
-        return formulierService.addNieuwFormulier(formulier);
+        System.out.println(formulier.getMaand());
+        System.out.println(formulier.getJaar());
+        System.out.println(formulier.getWerkDagen().toString());
+        //return formulierService.addNieuwFormulier(formulier);
+        return null;
     }
 
     @GetMapping("/all")
