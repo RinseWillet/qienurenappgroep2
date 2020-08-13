1f1e964b6133ff8b9f62243836343fa374ead6f0
Mauricepns <66377105+Mauricepns@users.noreply.github.com>
2020-08-13 11:58:24 +0200

    lombok niet mergen

--- src/main/java/app/qienuren/model/Bedrijf.java
+++ src/main/java/app/qienuren/model/Bedrijf.java
@@ -1,6 +1,8 @@
 package app.qienuren.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -11,99 +13,32 @@ public class Bedrijf {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter @Setter
     private long id;
     @OneToOne
+    @Getter @Setter
     private KlantContactPersoon contactPersoon;
+    @Getter @Setter
     private String naam;
+    @Getter @Setter
     private String emailadres;
+    @Getter @Setter
     private String telefoonnr;
+    @Getter @Setter
     private String straatNaamNr;
+    @Getter @Setter
     private String postcode;
+    @Getter @Setter
     private String woonplaats;
 
 
     @OneToMany(cascade = CascadeType.ALL)
     private List<Trainee> trainees = new ArrayList<>();
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public KlantContactPersoon getContactPersoon() {
-        return contactPersoon;
-    }
-
-    public void setContactPersoon(KlantContactPersoon contactPersoon) {
-        this.contactPersoon = contactPersoon;
-    }
-
-    public String getNaam() {
-        return naam;
-    }
-
-    public void setNaam(String naam) {
-        this.naam = naam;
-    }
-
-    public String getEmailadres() {
-        return emailadres;
-    }
-
-    public void setEmailadres(String emailadres) {
-        this.emailadres = emailadres;
-    }
-
-    public String getTelefoonnr() {
-        return telefoonnr;
-    }
-
-    public void setTelefoonnr(String telefoonnr) {
-        this.telefoonnr = telefoonnr;
-    }
-
-    public String getStraatNaamNr() {
-        return straatNaamNr;
-    }
-
-    public void setStraatNaamNr(String straatNaamNr) {
-        this.straatNaamNr = straatNaamNr;
-    }
-
-    public String getPostcode() {
-        return postcode;
-    }
-
-    public void setPostcode(String postcode) {
-        this.postcode = postcode;
-    }
-
-    public String getWoonplaats() {
-        return woonplaats;
-    }
-
-    public void setWoonplaats(String woonplaats) {
-        this.woonplaats = woonplaats;
-    }
-
-//    public String getNAWgegevens() {
-//        return NAWgegevens;
-//    }
-//
-//    public void setNAWgegevens(String NAWgegevens) {
-//        this.NAWgegevens = NAWgegevens;
-//    }
 
     @JsonIgnore
     public List<Trainee> getTrainees() {
         return trainees;
-    }
-
-    public void setTrainees(List<Trainee> trainees) {
-        this.trainees = trainees;
     }
 
     public void koppelTrainee (Trainee trainee) {
