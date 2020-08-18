7b0dcf9851a652e45ebe2bb429b5ad76afd36766
RinseWillet <r.willet@gmail.com>
2020-08-18 09:24:06 +0200

    dinsdag-ochtend, voor merge met meest up to date master

--- src/main/java/app/qienuren/templatecontroller/TemplateController.java
+++ src/main/java/app/qienuren/templatecontroller/TemplateController.java
@@ -30,4 +30,7 @@ public class TemplateController {
 
     @GetMapping("opdrachtgever")
     public String getOpdrachtgeverPage() { return "opdrachtgever";}
+
+    @GetMapping("traineeformulier")
+    public String getTraineeFormulier() { return "traineeformulier";}
 }
