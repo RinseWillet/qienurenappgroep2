f3bd7ae96fe2060c64dca423d82bd4951cec063e
Deniece <deniece.wijdenbosch@hotmail.com>
2020-09-08 11:41:13 +0200

    ff bean toegevoegd aan tijdelijke persoon

--- src/main/java/app/qienuren/controller/TijdelijkePersoonRepository.java
+++ src/main/java/app/qienuren/controller/TijdelijkePersoonRepository.java
@@ -2,6 +2,8 @@ package app.qienuren.controller;
 
 import app.qienuren.model.TijdelijkePersoon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TijdelijkePersoonRepository extends JpaRepository<TijdelijkePersoon, Long> {
 }
